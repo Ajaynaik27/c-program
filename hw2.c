f20802454e3654761fd2b06071c743c3e60ebddc
Ajay <141153320+Ajaynaik27@users.noreply.github.com>
2023-08-01 23:59:31 +0530

    Add files via upload

---
+++ hw2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/*write a program for multiplication table for given number*/
+int main()
+{
+    int n,i;
+    printf("enter the multiplication table of the number:\n");
+    scanf("%d",&n);
+    for (i=1;i<=10;i++);
+    {
+        printf("%d
+               *%d=%d\n",n,i,n*i);
+        i++;
+    }
+    return 0;
+}
